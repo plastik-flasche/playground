32ca752a2465a5858beac2c83ea4c50c6af2758f
plastik-flasche <plastikflasche@duck.com>
2023-11-25 18:04:16 +0100

    failing CI test

--- projects/calculator/calculator_test.py
+++ projects/calculator/calculator_test.py
@@ -7,7 +7,7 @@ class TestCalculator(unittest.TestCase):
         self.calc = Calculator()
 
     def test_add_method_returns_correct_result(self):
-        self.assertEqual(4, self.calc.add(2, 2))
+        self.assertEqual(3, self.calc.add(2, 2))
 
 
 if __name__ == '__main__':
