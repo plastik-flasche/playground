5d84973e36efcf8c406e75106696a9de17622c7e
plastik-flasche <plastikflasche@duck.com>
2023-11-26 16:53:29 +0100

    half added ts, added java

---
+++ projects/java_greeter/src/main/java/com/example/javagreeter/greeter/Greeter.java
@@ -0,0 +1,5 @@
+package com.example.javagreeter.greeter;
+
+public class Greeter {
+
+}
---
+++ projects/java_greeter/src/main/java/com/example/javagreeter/main/Main.java
@@ -0,0 +1,10 @@
+package com.example.javagreeter.main;
+
+import com.example.javagreeter.greeter.Greeter;
+import com.google.gson.Gson;
+
+class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
